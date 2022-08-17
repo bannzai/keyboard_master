24e22b58c3852cff58a6659ade2487d89bcb722b
bannzai <kingkong999yhirose@gmail.com>
2022-08-17 20:55:09 +0900

    WIP add keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.setPortrait();
+
+  runApp(const MyGame());
 }
 
 class MyGame extends StatelessWidget {
@@ -12,11 +18,77 @@ class MyGame extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GameWidget(game: KeyboardGame());
+    return Root();
+  }
+}
+
+class Root extends StatefulWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  late FocusNode _node;
+  late FocusAttachment _nodeAttachment;
+
+  @override
+  void initState() {
+    super.initState();
+    _node = FocusNode();
+    _node.addListener(() {
+      print("[DEBUG] changed");
+    });
+    _nodeAttachment = _node.attach(context, onKey: _handleKeyPress);
+  }
+
+  @override
+  void dispose() {
+    _node.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GameWidget(
+      game: KeyboardGame(),
+      focusNode: _node,
+      autofocus: true,
+    );
+  }
+
+  KeyEventResult _handleKeyPress(FocusNode node, RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      debugPrint(
+          'Focus node ${node.debugLabel} got key event: ${event.logicalKey}');
+      if (event.logicalKey == LogicalKeyboardKey.keyR) {
+        debugPrint('Changing color to red.');
+        return KeyEventResult.handled;
+      } else if (event.logicalKey == LogicalKeyboardKey.keyG) {
+        debugPrint('Changing color to green.');
+        return KeyEventResult.handled;
+      } else if (event.logicalKey == LogicalKeyboardKey.keyB) {
+        debugPrint('Changing color to blue.');
+        return KeyEventResult.handled;
+      }
+    }
+    return KeyEventResult.ignored;
   }
 }
 
 class KeyboardGame extends FlameGame with KeyboardEvents {
+  @override
+  Future<void> onLoad() async {
+    add(
+      Player()
+        ..position = size / 2
+        ..width = 50
+        ..height = 400
+        ..anchor = Anchor.center,
+    );
+  }
+
   // ...
   @override
   KeyEventResult onKeyEvent(
@@ -37,5 +109,14 @@ class KeyboardGame extends FlameGame with KeyboardEvents {
       return KeyEventResult.handled;
     }
     return KeyEventResult.ignored;
+  }
+}
+
+class Player extends PositionComponent {
+  static final _paint = Paint()..color = Colors.white;
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
   }
 }
